c7afb407761572eeff2ece19008d84a79267e31e
inf25dw1g13 <A046756@umaia.pt>
2026-01-10 20:41:17 +0000

    Delete src/food-delivery-lb4/dist directory

--- src/food-delivery-lb4/dist/repositories/pedidos-pratos-through.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { FooddbDataSource } from '../datasources';
-import { PedidosPratosThrough, PedidosPratosThroughRelations } from '../models';
-export declare class PedidosPratosThroughRepository extends DefaultCrudRepository<PedidosPratosThrough, typeof PedidosPratosThrough.prototype.id, PedidosPratosThroughRelations> {
-    constructor(dataSource: FooddbDataSource);
-}
